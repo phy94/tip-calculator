9f2cbadf0356a5b49f489050591e144c9848c768
Phyman Dang <phymandang@yahoo.com>
2023-03-20 15:27:54 -0700

    reformat to account for taxes

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function decimals(el) {
   el.value = parseFloat(el.value).toFixed(2);
 }
 
+function myChangeFunction(input1) {
+
+}
+
 function limitDecimal(el) {
   if (el.value.indexOf('.') > -1 && (el.value.length - el.value.indexOf('.') > 3)) {
     el.value = el.value.slice(0, -1);
@@ -9,6 +13,11 @@ function limitDecimal(el) {
 }
 
 var billAmount = document.getElementById("billAmount");
+var billTax = document.getElementById("billTax");
+
+billAmount.addEventListener('input', function () {
+  billTax.value = parseFloat(billAmount.value * 1.1).toFixed(2);
+});
 
 var invalidChars = [
   "-",
@@ -38,15 +47,16 @@ function update() {
   let bill = Number(document.getElementById("billAmount").value);
   let tipPercent = document.getElementById("tipSlider").value;
   let split = document.getElementById("splitInput").value;
+  let billWithTax = Number(document.getElementById("billTax").value);
 
   let tipValue = bill * (tipPercent / 100)
-  let splitEach = bill / split
+  let splitEach = billWithTax / split
   let tipEach = tipValue / split
-  let splitBillAmount = (bill + tipValue) / split
+  let splitBillAmount = (billWithTax + tipValue) / split
 
   document.getElementById("tipPercent").innerHTML = tipPercent + "%"
   document.getElementById("tipAmount").innerHTML = formatMoney(tipValue);
-  document.getElementById("totalWithTip").innerHTML = formatMoney(bill + tipValue);
+  document.getElementById("totalWithTip").innerHTML = formatMoney(billWithTax + tipValue);
   document.getElementById("splitWay").innerHTML = formatSplit(split);
   document.getElementById("splitEach").innerHTML = formatMoney(splitEach);
   document.getElementById("tipEach").innerHTML = formatMoney(tipEach);
